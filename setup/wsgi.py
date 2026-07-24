d68d1b50aaf92d86bcfa780a3172526ddf037d8a
Ivanildo-Lab <geficogestor@gmail.com>
2026-07-23 21:41:41 -0300

    Fix wsgi for PythonAnywhere with sys.path

--- setup/wsgi.py
+++ setup/wsgi.py
@@ -1,5 +1,9 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
+
+project = '/home/geficogestor/e-cobrancas'
+sys.path.insert(0, project)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
 
